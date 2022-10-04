e3c7cc84a9e1195b6f9c4058dbad652182994818
siddhantxh <113815634+siddhantxh@users.noreply.github.com>
2022-10-04 22:26:23 +0530

    Add files via upload

---
+++ IntroCard/co49d465ebf01828d46d20a8d/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+export default function Footer(){
+    return(
+        <div className="footer">
+        <a href="#" className="fa fa-facebook"></a>
+        <a href="#" className="fa fa-twitter"></a>
+        <a href="#" className="fa fa-linkedin"></a>
+        <a href="#" className="fa fa-android"></a>
+        </div>
+    )
+}
